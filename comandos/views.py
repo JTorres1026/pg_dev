47e57530421ba64f8ebdefe2b5f90285097d50dd
Juan Torres <juanestuardoje@gmail.com>
2024-11-01 10:08:36 -0600

    logica comandos

--- comandos/views.py
+++ comandos/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render, redirect
 from django.contrib.auth import authenticate, login
 from django.contrib import messages
 from django.contrib.auth.decorators import login_required
+from django.http import HttpResponse
 
 
 #Controlar el inicio de sesión
@@ -32,6 +33,25 @@ def login_view(request):
 def inicio_view(request):
     return render(request, 'inicio.html')
 
+
+
+
 @login_required
 def crear_contenedor_view(request):
+
+    if request.method == 'POST':
+        nombre_contenedor = request.POST.get('nombre_contenedor')
+        nombre_imagen = request.POST.get('nombre_imagen')
+        modo_ejecucion = request.POST.get('modo_ejecucion')
+        tiene_puerto = request.POST.get('tiene_puerto')
+        puerto_host = request.POST.get('puerto_host')
+        puerto_contenedor = request.POST.get('puerto_contenedor')
+                
+        print(nombre_contenedor,nombre_imagen,modo_ejecucion, tiene_puerto,puerto_host,puerto_contenedor)        
+        # procesar los datos o guardarlos en la base de datos
+        comando = 'El nombre de tu contenedor sera '+nombre_contenedor
+
+        
+        return render(request, 'contenedor/crear_contenedor.html', {'comando': comando })
+    
     return render(request, 'contenedor/crear_contenedor.html')
